c1bc54fc02ac2f89db7126af476e7b9debaec158
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-18 12:02:28 -0500

    Se modifico el metodo agregar mascotas que ya evalua si el propietario se encuentra registrado, tambien se modifico el modulo principal

--- bsd.py
+++ bsd.py
@@ -2,8 +2,9 @@
 #listas que almacenaran los ides de la mascota, propietario, veterinario
 
 ides_veterinarios=["1212121212"]
-ides_propietarios=[]
-ides_mascotas=["1212"]
+ides_propietarios=["1313131313"]
+ides_mascotas=["1212", "1313"]
+ides_visitas=[]
 #listas generales que almacenaran las informaciones de los actores
 
 
@@ -11,10 +12,19 @@ diccionario_mascota={
     "Id":"1212",
     "Nombre": "pepe",
     "Color": "2e",
-    "Especie":"",
-    "Raza":"",
-    "Veterinario":"",
-    "Historia Clinica":[]
+    "Especie":"gato",
+    "Raza":"angora",
+    "Veterinario":"32323",
+    "Historia Clinica":["122","1313"]
+}
+diccionario_mascota1={
+    "Id":"1313",
+    "Nombre": "pepe",
+    "Color": "negro",
+    "Especie":"gato",
+    "Raza":"angora",
+    "Veterinario":"32323",
+    "Historia Clinica":["433","313"]
 }
 diccionario={
             "Id":"1212121212",
@@ -25,6 +35,18 @@ diccionario={
             "Tarjeta profesional":"121212scxs",
             "Mascotas":["1212"]
         }
-lista_veterinarios =[diccionario]
-lista_mascota=[diccionario_mascota]
-lista_propietario =[]
+
+
+diccionario_propietario={
+    "Id":"1313131313",
+    "Nombre":"peddro",
+    "Apellidos":"herrera",
+    "Direccion":"algae",
+    "Telefono":"1313131",
+    "Correo":"qedas",
+    "Mascotas":["12123"]
+}
+lista_terinarios =[diccionario]
+lista_mascota=[diccionario_mascota,diccionario_mascota1]
+lista_propietario =[diccionario_propietario]
+lista_visitas=[]
