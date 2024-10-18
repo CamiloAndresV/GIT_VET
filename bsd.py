4611d257603a4b5e919def99a5f16038409a80f2
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-18 10:30:33 -0500

    Se agrego historia clinica

--- bsd.py
+++ bsd.py
@@ -1,10 +1,21 @@
 # modulo que contiene las listas generales
 #listas que almacenaran los ides de la mascota, propietario, veterinario
 
-ides_veterinarios=[]
+ides_veterinarios=["1212121212"]
 ides_propietarios=[]
-ides_mascotas=[]
+ides_mascotas=["1212"]
 #listas generales que almacenaran las informaciones de los actores
+
+
+diccionario_mascota={
+    "Id":"1212",
+    "Nombre": "pepe",
+    "Color": "2e",
+    "Especie":"",
+    "Raza":"",
+    "Veterinario":"",
+    "Historia Clinica":[]
+}
 diccionario={
             "Id":"1212121212",
             "Nombres":"junior",
@@ -12,8 +23,8 @@ diccionario={
             "Direccion":"Salgar",
             "Telefono":"1231131313",
             "Tarjeta profesional":"121212scxs",
-            "Mascotas":[]
+            "Mascotas":["1212"]
         }
-lista_veterinarios =[]
-lista_mascota=[]
+lista_veterinarios =[diccionario]
+lista_mascota=[diccionario_mascota]
 lista_propietario =[]
